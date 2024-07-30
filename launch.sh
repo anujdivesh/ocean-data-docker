042bfe519d4c3eeda664ac67df6d5983f026579a
anujdivesh <divesha@spc.int>
2024-07-31 11:12:38 +1200

    added health docker

--- launch.sh
+++ launch.sh
@@ -9,4 +9,4 @@ datasets_dir="/mnt/data/ocean_portal/datasets"
 
 docker run -v $(pwd)/code:/scripts \
 -v ${datasets_dir}:/home/pop/ocean_portal/datasets \
--ti --rm ocean-data -c 'python /scripts/main.py'
+-ti --rm ocean-data-download -c 'python /scripts/main.py'
